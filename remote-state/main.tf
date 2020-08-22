cab37496c864dbca2c14b07831b7ce1644d8aba5
baztian <bastian.dev@gmail.com>
2020-08-22 23:23:12 +0200

    Simplify code

--- remote-state/main.tf
+++ remote-state/main.tf
@@ -15,6 +15,14 @@ resource "aws_s3_bucket" "terraform_state" {
   lifecycle {
     prevent_destroy = true
   }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
 
 resource "aws_dynamodb_table" "terraform_state_lock" {
@@ -27,42 +35,6 @@ resource "aws_dynamodb_table" "terraform_state_lock" {
     name = "LockID"
     type = "S"
   }
-}
-
-resource "aws_s3_bucket_policy" "terraform_state" {
-  bucket = "${aws_s3_bucket.terraform_state.id}"
-  policy =<<EOF
-{
-  "Version": "2012-10-17",
-  "Id": "RequireEncryption",
-   "Statement": [
-    {
-      "Sid": "RequireEncryptedTransport",
-      "Effect": "Deny",
-      "Action": ["s3:*"],
-      "Resource": ["arn:aws:s3:::${aws_s3_bucket.terraform_state.bucket}/*"],
-      "Condition": {
-        "Bool": {
-          "aws:SecureTransport": "false"
-        }
-      },
-      "Principal": "*"
-    },
-    {
-      "Sid": "RequireEncryptedStorage",
-      "Effect": "Deny",
-      "Action": ["s3:PutObject"],
-      "Resource": ["arn:aws:s3:::${aws_s3_bucket.terraform_state.bucket}/*"],
-      "Condition": {
-        "StringNotEquals": {
-          "s3:x-amz-server-side-encryption": "AES256"
-        }
-      },
-      "Principal": "*"
-    }
-  ]
-}
-EOF
 }
 
 variable "stage" {}
