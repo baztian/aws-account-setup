a56f3472123baf674b50f497bb7a83b013c63148
Bastian Bowe <bastian.dev@gmail.com>
2020-11-25 20:18:17 +0100

    Set up hosted zone for zonny

---
+++ global/hosted-zone/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_route53_zone" "my_hosted_zone" {
+  name = var.domain_name
+  tags = {
+    Name : var.domain_name
+  }
+}
+
+resource "aws_acm_certificate" "my_certificate_request" {
+  domain_name               = var.domain_name
+  subject_alternative_names = ["*.{var.domain_name}"]
+  validation_method         = "DNS"
+
+  tags = {
+    Name : var.domain_name
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
---
+++ global/hosted-zone/vars.tf
@@ -0,0 +1,5 @@
+variable "domain_name" {
+    description = "Domain name to get control for"
+    type = string
+    default = "twenty.zonny.de"
+}
