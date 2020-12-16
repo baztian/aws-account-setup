1d4533b4ed2aa628502265c0f8bef7e872644e59
Bastian Bowe <bastian.dev@gmail.com>
2020-12-16 23:46:03 +0100

    Disable ECS CloudWatch Metrics to save money

--- dev/ecs/templates/user-data.sh
+++ dev/ecs/templates/user-data.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 # ECS config
+# see https://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-agent-config.html
 {
   echo "ECS_CLUSTER=${cluster_name}"
+# According to https://docs.aws.amazon.com/AmazonECS/latest/developerguide/cloudwatch-metrics.html
+# ECS metric data is automatically sent to CloudWatch in 1-minute periods. This costs extra money.
+# Therefore I'll disable it for now
+  echo "ECS_DISABLE_METRICS=true"
 } >> /etc/ecs/ecs.config
