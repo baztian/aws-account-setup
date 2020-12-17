592db0c7af030bf590b6f30d2b3ac79b50dde413
Bastian Bowe <bastian.dev@gmail.com>
2020-12-17 22:46:32 +0100

    Output name of the ALB

--- dev/alb/outputs.tf
+++ dev/alb/outputs.tf
@@ -2,6 +2,10 @@ output "alb_dns_name" {
   description = "DNS Name of the LB"
   value       = aws_lb.this_alb.dns_name
 }
+output "alb_name" {
+  description = "Name of the LB"
+  value       = aws_lb.this_alb.name
+}
 output "alb_http_listener_arn" {
   description = "arn of the created http alb_listeners"
   value = aws_lb_listener.http.arn
