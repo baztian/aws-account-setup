2a6514f5ced70043971784abf3628a4109a0c5cc
Bastian Bowe <bastian.dev@gmail.com>
2021-01-21 23:02:14 +0100

    Enable swap for ecs

--- dev/ecs/templates/user-data.sh
+++ dev/ecs/templates/user-data.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Setup 2 GB swap file
+# https://aws.amazon.com/premiumsupport/knowledge-center/ec2-memory-swap-file/
+dd if=/dev/zero of=/swapfile bs=128M count=16
+chmod 600 /swapfile
+mkswap /swapfile
+echo /swapfile swap swap defaults 0 0 >> /etc/fstab
+swapon -a
+
 # ECS config
 # see https://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-agent-config.html
 {
