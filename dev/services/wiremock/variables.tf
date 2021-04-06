39a077fb68f2f48a728cd96cee7a0327468b4152
baztian <bastian.dev@gmail.com>
2021-04-07 00:13:57 +0200

    Refactor wiremock ecs into a reusable module

--- dev/services/wiremock/variables.tf
+++ dev/services/wiremock/variables.tf
@@ -1,18 +1,3 @@
-variable "cluster_name" {
-  description = "The ECS cluster name"
-  type        = string
-  default = "complete-ecs"
-}
-variable "alb_name" {
-  description = "Name of the ALB to which we will assign a listener rule to forward to this service."
-  type = string
-  default = "www"
-}
 variable "wiremock_admin_password" {
   description = "Password to access the admin api for wiremock"
-}
-variable "environment" {
-  description = "The (isolated) environment of this service"
-  type = string
-  default = "dev"
 }
--- modules/services/wiremock-ecs/outputs.tf
+++ modules/services/wiremock-ecs/outputs.tf
@@ -1,12 +1,12 @@
 output "service_task_definition" {
-  value = module.wiremock-ecs.service_task_definition
+  value = aws_ecs_service.wiremock.task_definition
   description = "Task definition being used for the service"
 }
 output "image" {
-  value = module.wiremock-ecs.image
+  value = local.image
   description = "Name of the docker image being used for the service"
 }
 output "image_digest" {
-  value = module.wiremock-ecs.image_digest
+  value = data.docker_registry_image.wiremock.sha256_digest
   description = "SHA256 digest of the image being used for the service"
 }
