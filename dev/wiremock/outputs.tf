b48e2eb0fcc4205adf26bedfd0350fae0f002a0b
baztian <bastian.dev@gmail.com>
2021-04-08 23:53:45 +0200

    Minor improvements to ec2-wiremock

--- dev/wiremock/outputs.tf
+++ dev/wiremock/outputs.tf
@@ -1,16 +1,15 @@
-# Launch configuration
 output "this_launch_configuration_id" {
   description = "The ID of the launch configuration"
   value       = module.service_asg.this_launch_configuration_id
 }
-
-# Autoscaling group
 output "this_autoscaling_group_id" {
   description = "The autoscaling group id"
   value       = module.service_asg.this_autoscaling_group_id
 }
-
-# ALB DNS name
+output "this_autoscaling_group_name" {
+  value = module.service_asg.this_autoscaling_group_name
+  description = "Name of the autoscaling group that got created"
+}
 output "this_lb_dns_name" {
   description = "DNS Name of the LB"
   value       = data.aws_lb.www_lb.dns_name
